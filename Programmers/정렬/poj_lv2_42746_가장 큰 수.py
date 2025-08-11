3423406e6618d637f54aeb5b2ae98fd9168e9c46
seoyeon83 <mool848777@naver.com>
2025-08-11 17:43:25 +0900

    [Review] poj: 43165 - dfs

--- Programmers/정렬/poj_lv2_42746_가장 큰 수.py
+++ Programmers/정렬/poj_lv2_42746_가장 큰 수.py
@@ -1,30 +1,15 @@
-'''
-# 정수를 이어 붙여 만들 수 있는 가장 큰 수
-# 배열의 값을 모두 문자열로 만들고 내림차순 정렬하여 붙이면?
-
-아 근데 대부분 괜찮은데 아래의 경우가 문제	
-[3, 30, 34, 5, 9] -> 실행한 결괏값 "9534303"이 기댓값 "9534330"과 다릅니다.
-
-그러면 당장 비교하는 수가 같은데 한 수는 남은 자릿수가 없고, 다른 것은 나머지 자릿수가 있는 경우 세 가지 경우로 나뉨 
-3, 30 -> 330 (한 자릿수가 먼저) 
-3, 33 -> 333 (상관없음)
-3, 35 -> 353 (두 자릿수가 먼저)
-
-cmp_to_key를 활용해서 성공
-'''
 from functools import cmp_to_key
 
-def func(a, b):
+def cmp(a, b):
     if a+b > b+a:
-        return -1
+        return -1   # a가 앞에 와야 함
     if a+b < b+a:
-        return 1
-    
-    return 0 
+        return 1    # b가 앞에 와야 함
+    return 0        # 같음
     
 def solution(numbers):
     numbers_str = list(map(str, numbers))
-    numbers_str.sort(key=cmp_to_key(func))
+    numbers_str.sort(key=cmp_to_key(cmp))
     
     answer = ''.join(numbers_str)
     
