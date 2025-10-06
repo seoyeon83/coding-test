83ba32683c2c657665ea07f6dd6943abba8b3aa8
seoyeon83 <mool848777@naver.com>
2025-10-06 18:48:55 +0900

    [Review] poj 정렬 / 251006

--- Programmers/정렬/poj_lv2_42746_가장 큰 수.py
+++ Programmers/정렬/poj_lv2_42746_가장 큰 수.py
@@ -14,3 +14,18 @@ def solution(numbers):
     answer = ''.join(numbers_str)
     
     return '0' if answer[0] == '0' else answer
+
+
+
+# 다른 사람 풀이 참고해서 푼 것
+'''
+cmp 안 쓰고도 가능하네
+같은 수를 곱해서 ?
+3, 30, 34 는 34, 3, 30 으로 정렬되어야 이상적
+이 경우는 값을 *3해서 비교?
+'''
+
+def solution(numbers):
+    numbers_str = list(map(str, numbers))
+    numbers_str.sort(key=lambda x: x*3, reverse=True)
+    return str(int(''.join(numbers_str)))
