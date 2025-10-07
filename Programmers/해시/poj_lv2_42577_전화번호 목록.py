b79790be8ebab8b667061ee93378becd670e6bb8
seoyeon83 <mool848777@naver.com>
2025-10-08 01:34:35 +0900

    [Review] poj 해시 / 251007

---
+++ Programmers/해시/poj_lv2_42577_전화번호 목록.py
@@ -0,0 +1,22 @@
+# 처음 짠 코드 (효율성 테스트 통과 못함)
+def solution(phone_book):
+    # 길이로 정렬
+    phone_book.sort(key=lambda x: len(x))
+    
+    for i in range(len(phone_book)):
+        for j in phone_book[i+1:]:
+            if phone_book[i] == j[:len(phone_book[i])]:
+                return False
+            
+    return True
+
+# gemini 힌트를 보고 짠 코드 (접두어 활용)
+def solution(phone_book):
+    d = { x : True for x in phone_book }
+    
+    for x in phone_book:
+        for i in range(len(x)-1):
+            if x[:i+1] in d.keys():
+                return False
+    
+    return True
