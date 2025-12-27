361464edd1332cf2134df2909664250a5d8042b1
seoyeon83 <mool848777@naver.com>
2025-12-27 20:13:55 +0900

    [Add] 251227 / MySQL / 0.321s

--- solvesql/Advent of SQL 2025/solvesql_day16_lv3_초기 사용자의 친구 관계 찾기.sql
+++ solvesql/Advent of SQL 2025/solvesql_day16_lv3_초기 사용자의 친구 관계 찾기.sql
@@ -17,3 +17,20 @@ FROM
   ranking
 WHERE
   CAST(element_rank AS DECIMAL) / total_count <= 0.001;
+
+
+-- 251227 재시도!
+-- COUNT(*) OVER() 은 생각해내지 못했지만 그래도 성공했다
+WITH elements AS (
+  SELECT
+    user_a_id,
+    user_b_id,
+    user_a_id + user_b_id AS id_sum,
+    RANK() OVER(ORDER BY user_a_id + user_b_id ASC) AS ranking,
+    (SELECT COUNT(*) AS cnt FROM edges) AS cnt
+  FROM edges
+)
+
+SELECT user_a_id, user_b_id, id_sum
+FROM elements
+WHERE ranking / cnt <= 0.001;
