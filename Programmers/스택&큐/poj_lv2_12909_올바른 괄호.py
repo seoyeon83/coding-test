dc3143b2a061f9b14389c097253f3e6fe0873d03
seoyeon83 <mool848777@naver.com>
2026-01-20 18:06:19 +0900

    [Add] 250120 / Python

--- Programmers/스택&큐/poj_lv2_12909_올바른 괄호.py
+++ Programmers/스택&큐/poj_lv2_12909_올바른 괄호.py
@@ -5,19 +5,19 @@
 스택에 괄호가 남아있으면 false
 '''
 
-def solution(s):
-    stack = []
+# def solution(s):
+#     stack = []
     
-    for c in s:
-        if c == "(":
-            stack.append(c)
-        elif stack: # 닫힌 괄호이고 stack에 값이 있는 경우 
-            stack.pop()
-        else: # 닫힌 괄호이고 stack에 값이 없는 경우 -> false
-            return False
+#     for c in s:
+#         if c == "(":
+#             stack.append(c)
+#         elif stack: # 닫힌 괄호이고 stack에 값이 있는 경우 
+#             stack.pop()
+#         else: # 닫힌 괄호이고 stack에 값이 없는 경우 -> false
+#             return False
         
-    if stack: return False
-    return True
+#     if stack: return False
+#     return True
 
 
 '''
@@ -31,6 +31,9 @@ s 길이가 홀수 개면 False (짝이 안 맞으니까)
 s를 다 순회하고도 stack에 값이 남아있으면 False, 없으면 True
 
 첫 번째로 풀었을 때보다 깔끔한 것 같다!
+
+세 번재 조건 (not(stack) and c == ') 은 '()))' 과 같은 경우를 위한 것
+없어도 프로그래머스의 케이스에는 없어서 통과가 되긴 함
 '''
 
 def solution(s):
@@ -44,7 +47,11 @@ def solution(s):
             stack.append(c)
         elif stack and c == ')':
             stack.pop()
+        else: # not(stack) and c == ')
+            return False
     
     if stack:
         return False
     return True
+
+print(solution('()))'))
