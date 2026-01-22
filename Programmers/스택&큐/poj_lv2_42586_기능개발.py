44320ddbb48c92685a5978e24f6d440059e96645
seoyeon83 <mool848777@naver.com>
2026-01-22 18:42:51 +0900

    [Add] 260122 / Python 3 / 0.017ms / 9.208MB

--- Programmers/스택&큐/poj_lv2_42586_기능개발.py
+++ Programmers/스택&큐/poj_lv2_42586_기능개발.py
@@ -24,3 +24,83 @@ def solution(progresses, speeds):
         answer.append(cnt)
             
     return answer
+
+
+'''
+260122에 다시 풀이함
+'''
+
+from math import ceil
+
+def solution(progresses, speeds):
+    # 프로세스별 완성까지 걸리는 일 수 계산 
+    days = [ceil((100-progress)/speed) for progress, speed in zip(progresses, speeds)]
+
+    # 결과 도출
+    q = days[::-1]
+    answer, idx, day = [0], 0, q[-1]
+    while q:
+        cur_day = q.pop()
+        answer[idx] += 1
+        
+        # 더이상 그 날에 배포할 완료된 프로세스가 없음
+        if q and q[-1] > day:
+            idx += 1
+            answer.append(0)
+            day = q[-1]
+
+    return answer
+
+'''
+260122
+deque 활용
+'''
+from math import ceil
+from collections import deque
+
+def solution(progresses, speeds):
+    # 프로세스별 완성까지 걸리는 일 수 계산 
+    days = [ceil((100-progress)/speed) for progress, speed in zip(progresses, speeds)]
+
+    # 결과 도출
+    queue = deque(days)
+    answer, idx, day = [0], 0, queue[0]
+    while queue:
+        queue.popleft()
+        answer[idx] += 1
+        
+        # 더이상 그 날에 배포할 완료된 프로세스가 없음
+        if queue and queue[0] > day:
+            idx += 1
+            answer.append(0)
+            day = queue[0]
+
+    return answer
+
+
+'''
+260122
+첫 번째로 풀었던 방식과 합친 것
+=> 가장 이상적인 방식인듯!
+'''
+from math import ceil
+from collections import deque
+
+def solution(progresses, speeds):
+    # 프로세스별 완성까지 걸리는 일 수 계산 
+    days = [ceil((100-progress)/speed) for progress, speed in zip(progresses, speeds)]
+
+    # 결과 도출
+    queue = deque(days)
+    answer = list()
+    while queue:
+        n = queue.popleft()
+        cnt = 1
+        
+        while queue and n >= queue[0]:
+            queue.popleft()
+            cnt += 1
+        
+        answer.append(cnt)
+        
+    return answer
