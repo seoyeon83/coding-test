37370d67fe45a801842d270dbca626d2ba7db5b2
seoyeon83 <mool848777@naver.com>
2026-02-14 21:56:43 +0900

    [Add] 260214 / Python 3

---
+++ Programmers/완전탐색/poj_lv2_42839_소수찾기.py
@@ -0,0 +1,23 @@
+from itertools import permutations
+
+def solution(numbers):
+    # 에라토스테네스의 체로 소수 찾기
+    n = int(''.join(sorted(numbers, reverse=True)))
+    a = [False, False] + [True] * (n-1)
+    for i in range(2, n+1):
+        if a[i]:
+            for j in range(2*i, n+1, i):
+                a[j] = False
+                
+    # 순열로 만들 수 있는 조합을 찾고 소수인지 판별
+    primes = set()
+    for i in range(1, len(numbers)+1):
+        for comb in permutations(list(numbers), i):
+            c = int(''.join(comb))
+            if a[c]:
+                primes.add(c)
+    
+    return len(primes)
+
+
+print(solution("17"))
