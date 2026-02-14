e9ef97e7aab4cd9c0605116712b2cedac73673c6
seoyeon83 <mool848777@naver.com>
2026-02-15 01:17:33 +0900

    [Add] 260214 / Python 3

--- Programmers/완전탐색/poj_lv2_87946_피로도.py
+++ Programmers/완전탐색/poj_lv2_87946_피로도.py
@@ -16,3 +16,28 @@ def solution(k, dungeons):
             answer = cnt
             
     return answer
+
+
+
+'''
+260215
+'''
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = -1
+    
+    for perm in permutations(dungeons):
+        cur_k = k
+        cnt = 0
+        
+        for a, b in perm:
+            if a <= cur_k:
+                cur_k -= b
+                cnt += 1
+            
+        if cnt > answer:
+            answer = cnt
+            
+    return answer
