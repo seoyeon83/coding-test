e9a3696d27c963e3c2170cbee1a689763694ab52
seoyeon83 <mool848777@naver.com>
2026-02-14 22:42:02 +0900

    [Add] 260214 / Python 3

--- Programmers/완전탐색/poj_lv2_42839_소수찾기.py
+++ Programmers/완전탐색/poj_lv2_42839_소수찾기.py
@@ -4,7 +4,7 @@ def solution(numbers):
     # 에라토스테네스의 체로 소수 찾기
     n = int(''.join(sorted(numbers, reverse=True)))
     a = [False, False] + [True] * (n-1)
-    for i in range(2, n+1):
+    for i in range(2, int(n**0.5)+1):
         if a[i]:
             for j in range(2*i, n+1, i):
                 a[j] = False
@@ -12,8 +12,8 @@ def solution(numbers):
     # 순열로 만들 수 있는 조합을 찾고 소수인지 판별
     primes = set()
     for i in range(1, len(numbers)+1):
-        for comb in permutations(list(numbers), i):
-            c = int(''.join(comb))
+        for perm in permutations(list(numbers), i):
+            c = int(''.join(perm ))
             if a[c]:
                 primes.add(c)
     
