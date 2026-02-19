66738c388caa25adc0efdc92a7a53310962ee665
seoyeon83 <mool848777@naver.com>
2026-02-19 13:44:04 +0900

    [Add 260219 / Python 3

--- Programmers/dfs&bfs/poj_lv2_43165_타겟 넘버.py
+++ Programmers/dfs&bfs/poj_lv2_43165_타겟 넘버.py
@@ -16,3 +16,19 @@ def solution(numbers, target):
     return dfs(numbers, target, 0, 0)
 
 print(solution([4, 1, 2, 1], 4))
+
+
+'''
+260219
+혼자서 풀기 성공! ㅎㅎ
+'''
+
+def solution(numbers, target):
+    def dfs(n, i):
+        if i == len(numbers):
+            return 1 if n == target else 0
+        
+        return dfs(n + numbers[i], i+1) + \
+            dfs(n - numbers[i], i+1)
+
+    return dfs(0, 0)
