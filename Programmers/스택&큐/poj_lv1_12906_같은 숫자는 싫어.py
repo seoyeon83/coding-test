881ad17011e82880f6950192d0b547320690c2c7
seoyeon83 <mool848777@naver.com>
2026-06-09 17:22:50 +0900

    [Add] 260609 / Python 3

--- Programmers/스택&큐/poj_lv1_12906_같은 숫자는 싫어.py
+++ Programmers/스택&큐/poj_lv1_12906_같은 숫자는 싫어.py
@@ -7,3 +7,23 @@ def solution(arr):
             answer.append(n)
             
     return answer
+
+
+# 260609 2번째 풀이
+def solution(arr):
+    answer = arr[:1]
+    for a in arr[1:]:
+        if a == answer[-1]:
+            continue
+        answer.append(a)
+        
+    return answer
+
+# 260609 2번째 풀이 (개선)
+def solution(arr):
+    answer = arr[:1]
+    for a in arr[1:]:
+        if a != answer[-1]:
+            answer.append(a)
+            
+    return answer
