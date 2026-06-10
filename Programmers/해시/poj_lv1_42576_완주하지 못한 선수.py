302988a3eb66ed027480367dd8550396ddfd6793
seoyeon83 <mool848777@naver.com>
2026-06-10 10:33:02 +0900

    [Add] 260610 / Python 3

--- Programmers/해시/poj_lv1_42576_완주하지 못한 선수.py
+++ Programmers/해시/poj_lv1_42576_완주하지 못한 선수.py
@@ -17,3 +17,34 @@ def solution(participant, completion):
         
     return participant[-1]
     
+'''
+# 260610 재시도
+
+접근: kv 형태로 이름별 참가자 수를 세고, 이후 완주자 이름으로 숫자를 차감. 그 뒤에도 0 초과인 경우를 찾아 반환 (해시맵으로 count 관리)
+복잡도: O(n)
+개선: 
+    c[name] = c[name] + 1 if c.get(name, 0) else 1 
+    -> c[name] = c.get(name, 0) + 1 으로 로직 개선
+
+    c라고 간단히 정의한 변수명을 더 명확하게 하면 좋을듯
+    -> 혹은 collections.Counter를 사용하는 것도 방법! (와 이게된다고?)
+    
+    ```
+    from collections import Counter
+    
+    def solution(participant, completion):
+        diff = Counter(participant) - Counter(completion)
+        return next(iter(diff))
+    ```
+'''
+def solution(participant, completion):
+    c = dict()
+    for name in participant:
+        c[name] = c.get(name, 0) + 1
+    
+    for name in completion:
+        c[name] -= 1
+    
+    for k, v in c.items():
+        if v > 0:
+            return k
