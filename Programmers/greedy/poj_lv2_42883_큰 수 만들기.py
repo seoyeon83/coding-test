cf7ce17f17a75cc0399c69f6c0d6f26963aa9f90
seoyeon83 <mool848777@naver.com>
2026-07-02 23:04:34 +0900

    [Try] 260702 / Python 3

--- Programmers/greedy/poj_lv2_42883_큰 수 만들기.py
+++ Programmers/greedy/poj_lv2_42883_큰 수 만들기.py
@@ -20,4 +20,20 @@ print(solution("10", 1))
 # 260626
 생각한 접근은 좀 맞는 것 같은데 문제는 정확히 어떻게 구현해야 할지 잘 모르겠더라...
 지난 나의 풀이를 참고해 공부했으므로 별도 코드는 올리지 않겠다
+
+# 260702 
+흠.. 재시도했는데 이전에 이해를 잘 못했는지 제대로 못 풀었다 (스택을 잘 못쓰는 듯..)
+밑 코드는 공부용으로 기록한다
+
 '''
+
+def solution(number, k):
+    stack = []
+    for n in number:
+        while stack and k > 0 and stack[-1] < n:
+            stack.pop()      # 앞의 작은 걸 버림
+            k -= 1
+        stack.append(n)
+    if k > 0:                # 다 훑었는데 지울 게 남으면(내림차순이었던 경우)
+        stack = stack[:-k]   # 뒤에서 k개 자름
+    return ''.join(stack)
