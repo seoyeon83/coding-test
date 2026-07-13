e96272ed0592e28766a6d1fcb18d0678274d45b2
seoyeon83 <mool848777@naver.com>
2026-07-13 14:08:19 +0900

    [Add] 260713 / Python 3

--- Programmers/완전탐색/poj_lv2_87946_피로도.py
+++ Programmers/완전탐색/poj_lv2_87946_피로도.py
@@ -41,3 +41,51 @@ def solution(k, dungeons):
             answer = cnt
             
     return answer
+
+
+'''
+# 260713
+접근: 순열로 모든 경우를 구해서 탐색한다
+개선: 변수명, 생략 가능한 부분
+
+안그래도 풀 때 dfs로도 가능하겠다 싶었는데.. 다음에는 dfs로 해보면 좋겠다
+'''
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+    
+    for per in permutations(dungeons, len(dungeons)):
+        cnt, kk = 0, k
+        
+        for a, b in per:
+            if kk >= a: 
+                kk -= b
+                cnt += 1
+            else: break
+            
+        if cnt > answer:
+            answer = cnt
+        
+    return answer
+
+# 개선
+
+from itertools import permutations
+
+def solution(k, dungeons):
+    answer = 0
+    
+    for per in permutations(dungeons):
+        cnt, cur_k = 0, k
+        
+        for need, cost in per:
+            if cur_k >= need: 
+                cur_k -= cost
+                cnt += 1
+            else: break
+            
+        answer = max(cnt, answer)
+        
+    return answer
