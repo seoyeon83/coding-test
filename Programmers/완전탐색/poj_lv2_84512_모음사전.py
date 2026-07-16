57757705e3e676edc3d221d2af5a5e55893d082a
seoyeon83 <mool848777@naver.com>
2026-07-16 19:18:47 +0900

    [Add] 260715 / Python 3

--- Programmers/완전탐색/poj_lv2_84512_모음사전.py
+++ Programmers/완전탐색/poj_lv2_84512_모음사전.py
@@ -16,4 +16,15 @@ print(solution("AAAAE"))
 '''
 # 260715 
 다시 봐도 어떻게 푸는지 모르겠어서 지난 풀이를 보고 다시 따로 정리해봤다
+
+# 260716
+어제 풀었을 때 익힌 것을 기반으로 혼자 풀어봤다. 이제 이 풀이과정을 제대로 기록해둬야지
 '''
+def solution(word):
+    idx = {"A":0, "E":1, "I":2, "O":3, "U":4}
+    weight = [781, 156, 31, 6, 1]
+    
+    answer = 0
+    for i, c in enumerate(word):
+        answer += weight[i] * idx[c] + 1
+    return answer
